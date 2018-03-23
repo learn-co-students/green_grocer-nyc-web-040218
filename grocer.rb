b4de0132005dbda65fb8a2a3e0fc4ec22704f158
github username <github email address>
2018-03-23 11:49:38 -0400

    Done.

--- grocer.rb
+++ grocer.rb
@@ -1,15 +1,62 @@
+require 'pry'
+
 def consolidate_cart(cart)
   # code here
+  cart.uniq.each_with_object({}) do |item, new_cart|
+    name = item.keys.first
+    contents = item.values.first
+    contents[:count] = cart.count(item)
+    new_cart[name] = contents
+  end
 end
 
+# # clone the input cart
+# for each coupon
+# if the couponed item exists as a key in the cloned hash, extract it as a string(x) and add "W/ COUPON"(y)
+# Turn the extracted string(y) into a key in the cloned hash cart[y] = Hash.new,
+# now you have -- "AVOCADO W/ COUPON" => {}
+# cart[y].merge!(:price => 0, :clearance => true, :count => 0) (now I have the coupon hash defaults)
+# cart[y][:count] = cart[x][:count] % coupon[y][:num]
+# cart[y][:price] = (cost/num) * (cart[x][:count] - cart[y][:count])
+
 def apply_coupons(cart, coupons)
-  # code here
+  new_cart = {}
+  cart.each do |key, value|
+    value.each do |_k2, _v2|
+      coupons.each do |coupon|
+        x = "#{key} W/COUPON"
+        if key == coupon[:item] && cart[key][:count] >= coupon[:num] && !new_cart.include?(x)
+          cart[key][:count] -= coupon[:num]
+          new_cart[x] = { price: coupon[:cost], clearance: cart[key][:clearance], count: 1 }
+        elsif key == coupon[:item] && cart[key][:count] >= coupon[:num] && new_cart.include?(x)
+          cart[key][:count] -= coupon[:num]
+          new_cart[x][:count] += 1
+        end
+      end
+    end
+  end
+  cart.merge(new_cart)
 end
 
 def apply_clearance(cart)
-  # code here
-end
+  cart.each_with_object(cart) do |item|
+    clearance = item.last[:clearance]
+    price = item.last[:price]
+    item.last[:price] = price - (price * 0.20) if clearance
+  end
+  end
 
-def checkout(cart, coupons)
-  # code here
-end
+  def checkout(cart, coupons)
+    consolidated = consolidate_cart(cart)
+    coupons_applied = apply_coupons(consolidated, coupons)
+    clearance_applied = apply_clearance(coupons_applied)
+    total = 0
+    clearance_applied.each do | k, v |
+      sum = v[:price] * v[:count]
+      total += sum
+    end
+    if total > 100
+      total = total - (total * 0.10)
+    end
+    total
+  end
